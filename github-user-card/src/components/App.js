2bf62a9301ad08e093e5f8b3f47d72eb26c83934
Matthew Justice <matthewajustice@gmail.com>
2021-01-12 17:22:54 -0500

    Styling finished

--- github-user-card/src/components/App.js
+++ github-user-card/src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import Particles from 'react-particles-js';
 
 import UserCard from './UserCard';
 
@@ -20,18 +21,51 @@ class App extends Component {
       .catch((err) => console.log(err));
   }
 
+  clickHandler = () => {
+    this.setState({
+      expanded: !this.expanded,
+    });
+  };
+
   render() {
     return (
-      <div>
-        <h1>Hello World</h1>
-        {this.state.userData.length < 1 ? (
-          <h2>Loading...</h2>
-        ) : (
-          <UserCard
-            userData={this.state.userData}
-            userFollowers={this.state.userFollowers}
-          />
-        )}
+      <div className='wrapper'>
+        <div className='cardContainer'>
+          {this.state.userData.length < 1 ? (
+            <h2>Loading...</h2>
+          ) : (
+            <UserCard
+              userData={this.state.userData}
+              userFollowers={this.state.userFollowers}
+              expanded={this.state.expanded}
+              clickHandler={this.clickHandler}
+            />
+          )}
+        </div>
+        <Particles
+          id='particles-js'
+          params={{
+            particles: {
+              number: {
+                value: 150,
+              },
+              size: {
+                value: 3,
+              },
+              move: {
+                speed: 4,
+              },
+            },
+            interactivity: {
+              events: {
+                onhover: {
+                  enable: true,
+                  mode: 'repulse',
+                },
+              },
+            },
+          }}
+        />
       </div>
     );
   }
--- github-user-card/src/components/UserCard.js
+++ github-user-card/src/components/UserCard.js
@@ -1,31 +1,92 @@
 import React from 'react';
-import styled from 'styled-components';
+import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
-const UserCard = ({ userData, userFollowers }) => {
+const useStyles = makeStyles((theme) => ({
+  root: {
+    maxWidth: 345,
+    minWidth: 345,
+    backgroundColor: '#24292E',
+    color: 'whitesmoke',
+    marginTop: '20px',
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
+  followerText: {
+    paddingLeft: '10px',
+  },
+}));
+
+export default function UserCard({ userData, userFollowers }) {
+  const classes = useStyles();
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
   return (
-    <StyledUserCard>
-      <h2>{userData.login}</h2>
-      <img src={userData.avatar_url} alt='' width='150px'></img>
-      <section>
-        <h3>Name: {userData.name}</h3>
-        <h3>Location: {userData.location}</h3>
-        <h3>Followers:</h3>
-        {userFollowers.map((follower) => (
-          <h5 key={follower.id}>
-            <a href={follower.url}>{follower.login}</a>
-          </h5>
-        ))}
-      </section>
-    </StyledUserCard>
+    <Card className={classes.root}>
+      <CardHeader title={userData.login} />
+      <CardMedia
+        className={classes.media}
+        image={userData.avatar_url}
+        title='Paella dish'
+      />
+      <CardContent>
+        <Typography variant='body1' component='p'>
+          {userData.name}
+          <br />
+          {userData.location}
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <Typography className={classes.followerText}>Followers</Typography>
+        <IconButton
+          className={clsx(classes.expand, {
+            [classes.expandOpen]: expanded,
+          })}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label='show more'
+        >
+          <ExpandMoreIcon className='icon' />
+        </IconButton>
+      </CardActions>
+      <Collapse in={expanded} timeout='auto' unmountOnExit>
+        <CardContent>
+          <Typography paragraph>
+            {userFollowers.map((follower) => (
+              <h5 key={follower.id}>
+                <a href={follower.html_url} target='_blank' rel='noreferrer'>
+                  {follower.login}
+                </a>
+              </h5>
+            ))}
+          </Typography>
+        </CardContent>
+      </Collapse>
+    </Card>
   );
-};
-
-export default UserCard;
-
-const StyledUserCard = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  background-color: lightgray;
-  width: 20%;
-`;
+}
